171a3d2819ae9d329b152f835aec9b7741436c8e
julio.collado <julio.collado@overactive.com>
2021-07-20 07:38:50 -0300

    Initial Commit

---
+++ SwiftUIStuff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStuff
+//
+//  Created by Julio Collado on 20/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIStuff/SwiftUIStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStuffApp.swift
+//  SwiftUIStuff
+//
+//  Created by Julio Collado on 20/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
